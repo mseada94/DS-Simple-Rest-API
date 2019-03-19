07166d42e2407e0de0f88dd47a07ddbd715d9a63
Mohamed Seada <mohamed.seada.1994@gmail.com>
2019-03-19 22:49:52 +0200

    Deploy mongoDB Adaptor

---
+++ src/dbAdapter/mongoAdapter.js
@@ -0,0 +1,81 @@
+const { ObjectID, MongoClient } =require('mongodb');
+
+module.exports = function mongoAdapter(dbUrl, dbName){
+    
+    let dbClient, db;
+    
+    const connect = async function connectFn(){
+        dbClient = await MongoClient.connect(dbUrl,{ useNewUrlParser: true });
+        db = dbClient.db(dbName);
+    }
+
+    const close = async function closeFn(){
+        return dbClient.close();
+    }
+
+    const insert = async function insertDocument(collection, data){
+        try {
+            const result = await db.collection(collection)
+                .insertOne(data);
+
+            return result.ops[0];
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const update = async function updateDocument(collection, id, data){
+        try {
+            const result = await db.collection(collection)
+                .findOneAndUpdate({_id: new ObjectID(id)}, {$set: data}, {returnOriginal: false});
+            
+            return result.value;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const remove = async function removeDocument(collection, id){
+        try {
+            const result = await db.collection(collection)
+                .findOneAndDelete({_id: new ObjectID(id)}, {returnOriginal: false});
+            
+            return result.value;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const get = async function getDocument(collection, id){
+        try {
+            const result = await db.collection(collection)
+                .findOne({_id: new ObjectID(id)});
+            
+            return result;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const getAll = async function getAllDocument(collection, limit = 0, start = 0,){
+        try {
+            const result = await db.collection(collection)
+            .find({}, {limit, skip: start});
+            
+            
+            return result.toArray();
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    return {
+        connect,
+        close,
+        insert,
+        update,
+        remove,
+        get,
+        getAll
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 // Load environment variable from .env file
 require('dotenv').config();
+
+const mongoAdapter = require('./dbAdapter/mongoAdapter');
+
+const dbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017';
+const dbName = process.env.MONGO_DB_NAME || 'test';
+
+const dbAdapter = mongoAdapter(dbUrl, dbName);
+
+dbAdapter.connect().then(async _ =>{
+    let res;
+    res = await dbAdapter.getAll('stores',3, 5);
+    console.log(res);
+
+    res = await dbAdapter.insert('stores', {name:'test'});
+    console.log(res);
+
+    res = await  dbAdapter.get('stores', res._id);
+    console.log(res);
+    
+    res = await dbAdapter.update('stores', res._id, {name:'updated'});
+    console.log(res);
+
+    res = await  dbAdapter.remove('stores', res._id);
+    console.log(res);
+    
+    dbAdapter.close();
+});
